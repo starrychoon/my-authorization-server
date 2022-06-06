40cd1cc74bd8cd1d674a482bed883d438e2cfe49
박성춘 <starrychoon@gmail.com>
2022-06-06 20:51:24 +0900

    :arrow_up: upgrade dependencies (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ import org.jlleitschuh.gradle.ktlint.reporter.*
 plugins {
     kotlin("jvm")
     id("org.jlleitschuh.gradle.ktlint")
+
+    id("io.spring.dependency-management")
 }
 
 allprojects {
@@ -26,6 +28,7 @@ allprojects {
 
 subprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "io.spring.dependency-management")
 
     group = "io.starrychoon"
     version = "1.0-SNAPSHOT"
@@ -40,6 +43,32 @@ subprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+
+    dependencyManagement {
+        val coroutines_version: String by project
+        val kotlin_logging_version: String by project
+        val spring_authorization_server_version: String by project
+        val bouncy_castle_version: String by project
+
+        dependencies {
+            dependencySet("org.jetbrains.kotlinx:$coroutines_version") {
+                entry("kotlinx-coroutines-core-jvm")
+                entry("kotlinx-coroutines-reactive")
+                entry("kotlinx-coroutines-reactor")
+            }
+            dependency("io.github.microutils:kotlin-logging-jvm:$kotlin_logging_version")
+
+            dependency(
+                mapOf(
+                    "group" to "org.springframework.security",
+                    "name" to "spring-security-oauth2-authorization-server",
+                    "version" to spring_authorization_server_version,
+                ),
+            )
+
+            dependency("org.bouncycastle:bcpkix-jdk15on:$bouncy_castle_version")
+        }
     }
 }
 
